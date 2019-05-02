52bf856a1ef893a58bddd0f95825722cef4ea636
fcorencoret <frencoret@uc.cl>
2019-05-02 15:20:05 -0400

    Small path fix for train_model

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -5,8 +5,9 @@
 #
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
-sys.path.insert(0, os.path.abspath('./clevr-iep'))
+
 import sys
+sys.path.insert(0, os.path.abspath('./clevr-iep'))
 import os
 
 import argparse
